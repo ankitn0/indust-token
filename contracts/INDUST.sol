d25725c09367e95983e1c7c87b7c3a4c356edee8
ankitn0 <ankit.an.namdeo@gmail.com>
2025-04-09 17:58:10 +0530

    Fix abstract contract issues and add proper overrides

--- contracts/INDUST.sol
+++ contracts/INDUST.sol
@@ -1,21 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
-contract INDUST is ERC20, ERC20Burnable, ERC20Pausable, Ownable {
-    uint256 public taxPercentage = 2; // 2% transfer tax
+contract INDUST is ERC20, ERC20Burnable, ERC20Pausable, Ownable, ERC20Permit, ERC20Votes {
     address public rewardPool;
-    uint256 private constant INITIAL_SUPPLY = 1_000_000_000_000 * 10 ** 18;
+    uint256 public transferTax = 5; // 5%
+    address public paymentReceiver;
 
-    event Payment(address indexed from, address indexed to, uint256 amount, string message);
-
-    constructor(address _rewardPool) ERC20("INDUST", "INDUST") {
-        _mint(msg.sender, INITIAL_SUPPLY);
+    constructor(address _rewardPool)
+        ERC20("INDUST", "INDUST")
+        ERC20Permit("INDUST")
+    {
         rewardPool = _rewardPool;
+        paymentReceiver = msg.sender;
+        _mint(msg.sender, 1_000_000_000_000 * 10 ** decimals()); // 1T tokens
     }
 
     function pause() public onlyOwner {
@@ -26,36 +29,55 @@ contract INDUST is ERC20, ERC20Burnable, ERC20Pausable, Ownable {
         _unpause();
     }
 
-    function setTax(uint256 _taxPercentage) public onlyOwner {
-        require(_taxPercentage <= 10, "Tax too high");
-        taxPercentage = _taxPercentage;
+    function setTransferTax(uint256 tax) public onlyOwner {
+        require(tax <= 10, "Max tax is 10%");
+        transferTax = tax;
     }
 
-    function setRewardPool(address _rewardPool) public onlyOwner {
-        rewardPool = _rewardPool;
+    function setPaymentReceiver(address receiver) public onlyOwner {
+        paymentReceiver = receiver;
     }
 
-    function mint(address to, uint256 amount) public onlyOwner {
-        _mint(to, amount);
+    function pay(address to, uint256 amount) public {
+        _transfer(msg.sender, paymentReceiver, amount);
+        emit Paid(msg.sender, to, amount);
     }
 
-    function pay(address to, uint256 amount, string memory message) public {
-        _transferWithTax(msg.sender, to, amount);
-        emit Payment(msg.sender, to, amount, message);
+    event Paid(address from, address to, uint256 amount);
+
+    // OVERRIDES
+
+    function _update(address from, address to, uint256 value)
+        internal
+        override(ERC20, ERC20Pausable, ERC20Votes)
+    {
+        super._update(from, to, value);
     }
 
-    function _transfer(address from, address to, uint256 amount) internal override {
-        _transferWithTax(from, to, amount);
+    function _transfer(address from, address to, uint256 amount)
+        internal
+        override
+    {
+        if (transferTax > 0 && from != owner() && to != owner()) {
+            uint256 taxAmount = (amount * transferTax) / 100;
+            super._transfer(from, rewardPool, taxAmount);
+            super._transfer(from, to, amount - taxAmount);
+        } else {
+            super._transfer(from, to, amount);
+        }
     }
 
-    function _transferWithTax(address from, address to, uint256 amount) internal {
-        require(!paused(), "Token is paused");
+    function _mint(address to, uint256 amount)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._mint(to, amount);
+    }
 
-        uint256 tax = (amount * taxPercentage) / 100;
-        uint256 amountAfterTax = amount - tax;
-
-        super._transfer(from, rewardPool, tax);
-        super._transfer(from, to, amountAfterTax);
+    function _burn(address from, uint256 amount)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._burn(from, amount);
     }
 }
-
